981395716d9af02acfed34e3fcc29c20ae83e469
Juan Sanchez <juandi.jds10@gmail.com>
2025-11-29 22:55:29 -0500

    Fix hooks, links, eslint build errors

--- pages/music.js
+++ pages/music.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-export default function mUs1c() {
+export default function MUs1c() {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
